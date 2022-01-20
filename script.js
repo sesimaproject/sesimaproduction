9b8ec0ed0781bda85441776cb477f6b48db6786f
Gobousei <93245017+Gobousei@users.noreply.github.com>
2022-01-20 15:18:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //画面に画像を表示する
 document.write( '<img id="rico" src="https://user-images.githubusercontent.com/93245017/150281128-724ce237-09c5-4d4b-8753-1ee71c0c478b.png">' );
- 
+ docment.write("わっはは");
 
